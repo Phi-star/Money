fadcee37677eeab394a52500e18be688ca064005
Phi-star <saliuphilip8@gmail.com>
2025-03-19 18:34:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 const domain = process.env.DOMAIN || 'http://localhost'; // Default to localhost if DOMAIN is not set
 
+// Redirect only URLs that contain "?"
+app.use((req, res, next) => {
+    if (req.originalUrl.includes('?')) {
+        console.log(`Redirecting: ${req.originalUrl} to /`);
+        return res.redirect('/');
+    }
+    next();
+});
+
 // Serve static files from the current directory (home directory)
 app.use(express.static(__dirname));
 
 // Log the domain when the server starts
 console.log(`Website will be deployed at ${domain}:${port}`);
-
-// Redirect only URLs that contain "?"
-app.use((req, res, next) => {
-    if (req.url.includes('?')) {
-        console.log(`Redirecting: ${req.url} to /`);
-        return res.redirect('/');
-    }
-    next();
-});
 
 // For any route, send the 'index.html' file as a fallback
 app.get('*', (req, res) => {
